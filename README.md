918dc52cf368ec0ce2a5b9c5216aac94d75389fa
E Jordan Gottlieb <ejordangottlieb@gmail.com>
2015-02-09 14:54:35 -0700

    version .13 psidoffset=0 fix, attribution, constructor cleanup

---
+++ README.md
@@ -0,0 +1,17 @@
+pyswmap
+=======
+
+This is a IETF Softwires Working Group MAP (MAP-E and MAP-T) Python 3 module.  It currently accepts BMR entry in using either or both "Sharing Ratio" and "EA length."  It can also calculate the PSID by BMR and session IPv4 port.  Session end-user IPv6 PD and MAP CE address can be calculated by supplied BMR, IPv4 session port, and IPv4 session address.
+ 
+ACKNOWLEDGEMENTS
+================
+
+This code has been inspired or uses some parts of the following:
+
+* Cisco MAP Simulation Tool
+
+  http://map46.cisco.com
+
+* Stateless NAT46 Linux kernel module implementation by Andrew Yourchenko
+
+  https://github.com/ayourtch/nat46
--- pyswmap.py
+++ pyswmap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # There is still a great deal of work required on this module.  Please
-# use with caution.
+# use with caution.  
 # -Jordan 
 
 import sys
@@ -17,11 +17,78 @@ from math import (
 
 class MapCalc(object):
 
-    def __init__(self,rulev6,rulev4):
-        self.ealen = False
-        self.psidoffset = False
-        self.ratio = False
+    def __init__(self,**bmr):
+        #rulev6,rulev4):
         self.portranges = False
+
+        # Validate and set BMR and BMR derived values 
+        self._check_bmr_values(bmr)
+
+    def _check_bmr_values(self,bmr):
+        # Assume these values have not been supplied.  Validate later.
+        self.ealen = False
+        self.ratio = False
+
+        # Validate that a proper PSID Offset has been set
+        if 'psidoffset' not in bmr:
+            print("The PSID offset has not been set")
+            sys.exit(1)
+        else:
+            self.psidoffset = self._psid_offset(bmr['psidoffset'])
+
+        # Validate that a proper IPv4 rule prefix is defined
+        if 'rulev4' not in bmr:
+            print("The rule IPv4 prefix has not been set")
+            sys.exit(1)
+        else:
+            self.rulev4 = self._ipv4_rule(bmr['rulev4'])
+
+        # Validate that a proper IPv6 rule prefix is defined
+        if 'rulev6' not in bmr:
+            print("The rule IPv6 prefix has not been set")
+            sys.exit(1)
+        else:
+            self.rulev6 = self._ipv6_rule(bmr['rulev6'])
+
+        # Check if EA length was passed
+        if 'ealen' not in bmr:
+            self.ealen = False
+        else:
+            print("EA length was passed")
+            self.ealen = bmr['ealen']
+            self.ratio = self._calc_ratio(bmr['ealen'])
+
+        # Check if sharing ratio was passed or calculated by _calc_ratio
+        if 'ratio' not in bmr:
+            # Skip if we have already calculated ratio
+            if not (self.ratio):
+                self.ratio = False
+        else:
+            print("Sharing ratio was passed")
+            if (self.ealen):
+                # Check to see if supplied EA length contradicts supplied ratio
+                if ( bmr['ratio'] != self.ratio ):
+                    eavalue = "EA value {}".format(self.ealen)
+                    sharingratio = "sharing ratio {}".format(bmr['ratio'])
+                    print("Supplied {} and {} are contradictory".format(
+                                                                  eavalue,
+                                                                  sharingratio)
+                         )
+                    sys.exit(1)
+                print("Sharing ratio calculated properly")
+            else:
+                self.ratio = bmr['ratio']
+                self.ealen = self._calc_ea(bmr['ratio'])
+
+        # EA length or sharing ratio must be set
+        if not ( self.ealen or self.ratio):
+            print("The BMR must include an EA length or sharing ratio")
+            sys.exit(1)
+
+        # Since we have not hit an exception we can calculate the port bits
+        self.portbits = self._calc_port_bits()
+
+    def _ipv4_rule(self,rulev4):
         try:
             self.rulev4mask = ip_network(
                         rulev4,
@@ -30,6 +97,10 @@ class MapCalc(object):
         except ValueError: 
             print("Invalid IPv4 prefix {}".format(rulev4))
             sys.exit(1)
+
+        return rulev4
+
+    def _ipv6_rule(self,rulev6):
         try:
             self.rulev6mask = IPv6Network(
                         rulev6,
@@ -39,8 +110,15 @@ class MapCalc(object):
             print("Invalid IPv6 prefix {}".format(rulev6))
             sys.exit(1)
 
-        self.rulev4 = rulev4
-        self.rulev6 = rulev6
+        return rulev6
+
+    def _psid_offset(self,psidoffset):
+        PSIDOFFSET_MAX = 6
+        if psidoffset in range(0,PSIDOFFSET_MAX+1):
+            return psidoffset
+        else:
+            print("Invalid PSID Offset value: {}".format(psidoffset))
+            sys.exit(1)
 
     def _psid_range(self,x):
         rset = []
@@ -48,34 +126,39 @@ class MapCalc(object):
             rset.append(2**i)
         return rset
 
-    def calc_ea(self):
-        if self.ratio not in ( self._psid_range(16) ):
+    def _calc_port_bits(self):
+        portbits = 16 - self.psidoffset - self.psidbits
+        return portbits
+
+    def _calc_ea(self,ratio):
+        if ratio not in ( self._psid_range(16) ):
             sys.exit(1)
 
-        if ( 1 == self.ratio):
+        if ( 1 == ratio):
             self.psidbits = 0
         else:
-            self.psidbits = int(log(self.ratio,2))
-        self.ealen = self.psidbits + ( 32 - self.rulev4mask )
-        self.portbits = 16 - self.psidoffset - self.psidbits
+            self.psidbits = int(log(ratio,2))
+        ealen = self.psidbits + ( 32 - self.rulev4mask )
+        return ealen
 
-    def calc_ratio(self):
-        self.psidbits = self.ealen - ( 32 - self.rulev4mask )
-        self.ratio = 2**self.psidbits
-        self.portbits = 16 - self.psidoffset - self.psidbits
+    def _calc_ratio(self,ealen):
+        self.psidbits = ealen - ( 32 - self.rulev4mask )
+        ratio = 2**self.psidbits
+        return ratio
 
     def gen_psid(self,portnum):
         if ( portnum < self.start_port() ):
             print("port value is less than allowed by PSID Offset")
             sys.exit(1)
-        self.psid = (portnum & ((2**self.psidbits - 1) << self.portbits)) \
-                    >> self.portbits
+        self.psid = (portnum & ((2**self.psidbits - 1) << self.portbits)) 
+        self.psid = self.psid >> self.portbits
         return self.psid
 
     def port_ranges(self):
         return 2**self.psidoffset - 1
 
     def start_port(self):
+        if self.psidoffset == 0: return 0  
         return 2**(16 - self.psidoffset)
 
     def port_list(self):
@@ -83,6 +166,7 @@ class MapCalc(object):
         increment = (2**self.psidbits) * (2**self.portbits)
         portlist = [ ]
         for port in range(startrange,startrange + 2**self.portbits):
+            if port >= 65536: continue
             portlist.append(port)
         for x in range(1,self.port_ranges()):
             startrange += increment
@@ -120,23 +204,25 @@ class MapCalc(object):
 if __name__ == "__main__":
     # A quick example showing current module capabilities:
     # The next four lines are used to supply the BMR
-    # The MAP IPv4 and IPv6 rule prefixes
-    m = MapCalc("fd80::/48","24.50.100.0/24")
-    # Set the PSID offset utilized in the forthcoming calculations
-    m.psidoffset = 6
-    # Supply ratio instead of EA length.  Either will work
-    m.ratio = 64
-
-    # Ratio was given so calculate EA length.  Also calc PSID and Port Bits
-    m.calc_ea()
-
-    # Using EA length value instead of ratio
-    # m.ealen = 5
-    # m.calc_ratio()
+    # 1.  The IPv6 rule prefix: rulev6      (a string) 
+    # 2.  The IPv4 rule prefix: rulev4      (a string)
+    # 3.  The PSID Offset:      psidoffset  (an integer)
+    #
+    # One of the two following values:
+    # 4a. The Sharing Ratio:    ratio       (an integer)
+    #                    or
+    # 4b. The EA Length:        ealen       (an integer)
+    # 
+    m = MapCalc( rulev6='fd80::/48',
+                 rulev4='24.50.100.0/24',
+                 psidoffset=6,
+                 ratio=64,
+                 #ealen=14,
+               )
 
     # Supply arbitrary layer-4 port that is valid given PSID Offset used
-    # to calculate the PSID.
-    portvalue = 1024
+    # to calculate the PSID.  PSID is stored in m.psid
+    portvalue = 40000
     m.gen_psid(portvalue)
 
     # Supply an IPv4 address from IPv4 rule prefix and use it and the 
@@ -146,7 +232,7 @@ if __name__ == "__main__":
     sharedv4 = "24.50.100.100"
     m.gen_mapaddr(m.ipv4_index(sharedv4))
 
-    # Print out some of the pertinent information
+    # Print out some of the pertinent user supplied and calculated values
     print("\n\n")
     print("################################################")
     print("BMR:")
@@ -175,6 +261,3 @@ if __name__ == "__main__":
     raw_input("Press the ENTER/RETURN key to continue")
     print("\n")
     print(m.port_list())
-    
-
-    
