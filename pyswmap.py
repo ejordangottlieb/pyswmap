d0f1a045c7f05d9089009794db8c8c21d0430129
E Jordan Gottlieb <ejordangottlieb@gmail.com>
2015-02-11 08:15:40 -0700

    version .15 additional documentation

--- pyswmap.py
+++ pyswmap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # There is still a great deal of work required on this module.  Please
 # use with caution.  
@@ -202,7 +202,8 @@ class MapCalc(object):
 
 if __name__ == "__main__":
     # A quick example showing current module capabilities:
-    # The next four lines are used to supply the BMR
+    # We create a new instance of class MapCalc and supply the BMR
+    # with the following values:
     # 1.  The IPv6 rule prefix: rulev6      (a string) 
     # 2.  The IPv4 rule prefix: rulev4      (a string)
     # 3.  The PSID Offset:      psidoffset  (an integer)
@@ -212,7 +213,38 @@ if __name__ == "__main__":
     # 4a. The Sharing Ratio:    ratio       (an integer)
     #                    or
     # 4b. The EA Length:        ealen       (an integer)
-    # 
+    #
+    # This will result in the both calculated and validated class variables:
+    #
+    #                  m.rulev4:     The IPv4 rule prefix used by a particular
+    #                                mapping rule.
+    #
+    #                  m.rulev6:     The IPv6 rule prefix used by a particular
+    #                                mapping rule.
+    #
+    #                  m.rulev4mask: The number of bits in the IPv4 rule subnet
+    #                                mask.
+    #
+    #                  m.rulev6mask: The number of bits in the IPv6 rule subnet
+    #                                mask.
+    #
+    #                  m.ealen:      The number of Embedded Address (EA) bits.
+    #
+    #                  m.ratio:      The sharing ratio of # of users per IPv4
+    #                                address.  This is 2 to the power of bits
+    #                                in the PSID field.
+    #
+    #                  m.psidoffset: The PSID Offset value.  Defined as the
+    #                                "A" field in the IETF MAP specification.
+    #
+    #                  m.portbits:   The number of contiguous ports as defined
+    #                                by the "m bits" in the IETF MAP 
+    #                                specification.
+    #
+    #                  m.psidbits:   The length in bits of the PSID field.  It
+    #                                is defined as the "k bits" in the IETF MAP
+    #                                specification.
+    #
     m = MapCalc( rulev6='fd80::/48',
                  rulev4='24.50.100.0/24',
                  #psidoffset=6,
@@ -220,31 +252,31 @@ if __name__ == "__main__":
                  #ealen=14,
                )
 
-    # Supply arbitrary layer-4 port that is valid given PSID Offset. Resulting
-    # in the calculation of the following:
-    #                  m.psid:      The port-set ID which defines the
-    #                               algorithmically assigned ports unique to
-    #                               a particular MAP CE.
+    # Supply arbitrary layer-4 port that is valid given PSID Offset to 
+    # gen_psid method.  This will calculate the following values:
+    #                  m.psid:       The port-set ID which defines the
+    #                                algorithmically assigned ports unique to
+    #                                a particular MAP CE.
     #
-    #                  m.port_list: The full list of ports assigned by a 
-    #                               particular PSID.  This attribute is
-    #                               a Python list.
+    #                  m.port_list:  The full list of ports assigned by a 
+    #                                particular PSID.  This attribute is
+    #                                a Python list.
     portvalue = 40000
     m.gen_psid(portvalue)
 
-    # Supply an IPv4 address from IPv4 rule prefix and PSID and 
-    # use them to calculate:
-    #                  m.pd:        The end-user IPv6 prefix. Typically,
-    #                               but not exclusively DHCPv6 PD.  Can
-    #                               also be assigned via SLAAC or configured
-    #                               manually.  
+    # Supply an IPv4 address from IPv4 rule prefix and PSID to gen_mapaddr
+    # method and use them to calculate the following values:
+    #                  m.pd:         The end-user IPv6 prefix. Typically,
+    #                                but not exclusively DHCPv6 PD.  Can
+    #                                also be assigned via SLAAC or configured
+    #                                manually.  
     #                      
-    #                  m.mapce:     The MAP IPv6 address.  This address
-    #                               is used to reach the MAP functions
-    #                               of a provisioned/configured MAP CE.
+    #                  m.mapce:      The MAP IPv6 address.  This address
+    #                                is used to reach the MAP functions
+    #                                of a provisioned/configured MAP CE.
     #                                  
-    #                               Detailed definitions are available
-    #                               in draft-ietf-softwire-map.
+    #                                Detailed definitions are available
+    #                                in draft-ietf-softwire-map.
     sharedv4 = '24.50.100.100'
     m.gen_mapaddr(sharedv4,m.psid)
 
@@ -276,4 +308,5 @@ if __name__ == "__main__":
     raw_input = vars(__builtins__).get('raw_input',input)
     raw_input("Press the ENTER/RETURN key to continue")
     print("\n")
+    # Print out list of ports for session PSID
     print(m.port_list)
